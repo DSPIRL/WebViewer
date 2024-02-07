e37c59f8fb4974b4a46c0bc9fc021ea0913cb85f
Raphi <71217268+DSPIRL@users.noreply.github.com>
2024-02-07 14:41:43 -0500

    Add files via upload

---
+++ WebViewer/WebViewerApp.swift
@@ -0,0 +1,17 @@
+//
+//  WebViewerApp.swift
+//  WebViewer
+//
+//  Created by Raphael Erfan on 2/7/24.
+//
+
+import SwiftUI
+
+@main
+struct WebViewerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
